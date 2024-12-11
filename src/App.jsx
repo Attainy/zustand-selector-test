5bcf2c38820065c13cc0bc7da64e73e7fd04176e
Attainy <attainy00@gmail.com>
2024-12-12 03:02:10 +0900

    createState가 없는 경우

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,24 @@ import useStore from './useStore'; // Zustand 스토어 import
 
 const App = () => {
   const states = useStore(); // count 상태 구독
-  const setCount = useStore((state) => state.setCount); // setCount 함수 구독
+  console.log(states)
+  // const setCount = useStore((selector) => selector.setCount); // setCount 함수 구독
 
-  return (
-    <>
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>state: {JSON.stringify(states)}</h1>
-      <button onClick={setCount}>Increment and Replace State</button>
-    </div>
-    <div>
-      <h1>Zustand Selector Test</h1>
-      <Actions />
-      <FullStateSubscriber />
-      <CountSubscriber />
-    </div>
-    </>
-  );
+  // return (
+  //   <>
+  //   <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      // <h1>state: {JSON.stringify(states)}</h1>
+  //     <button onClick={setCount}>Increment and Replace State</button>
+  //   </div>
+  //   <div>
+  //     <h1>Zustand Selector Test</h1>
+  //     <Actions />
+  //     <FullStateSubscriber />
+  //     <CountSubscriber />
+  //   </div>
+  //   </>
+  // );
+  return <div>가나다</div>
 };
 
 export default App;
--- src/useStore.js
+++ src/useStore.js
@@ -2,19 +2,22 @@ import { create } from 'zustand';
 
 console.log(`create: \n\n${create()}`)
 
-const useStore = create((set, get, getInitialState) => {
-  console.log(`set :\n\n${set}`)
-  console.log(`get :\n\n${get}`)
-  console.log(`init :\n\n${getInitialState}`)
+// const useStore = create((set, get, getInitialState) => {
+//   console.log(`set :\n\n${set}`)
+//   console.log(`get :\n\n${get}`)
+//   console.log(`init :\n\n${getInitialState}`)
 
-  return {
-  count: 0, // 상태 1
-  text: '', // 상태 2
-  increaseCount: () => set((state) => ({ count: state.count + 1 })), // count 증가
-  updateText: (newText) => set({ text: newText }), // text 업데이트
-  // 함수로 상태 변경 및 교체
-  setCount: () =>
-  set((state) => ({ count: state.count + 1 }), true),
-}});
+//   return {
+//   count: 0, // 상태 1
+//   text: '', // 상태 2
+//   increaseCount: () => set((state) => ({ count: state.count + 1 })), // count 증가
+//   updateText: (newText) => set({ text: newText }), // text 업데이트
+//   // 함수로 상태 변경 및 교체
+//   setCount: () =>
+//   set((state) => ({ count: state.count + 1 }), true),
+// }});
+
+const useStore = create();
+console.log('store', useStore())
 
 export default useStore;
