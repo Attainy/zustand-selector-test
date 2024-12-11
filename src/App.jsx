fbc9d0c5d2b042c207442b394742bfb4ff75adcb
Attainy <attainy00@gmail.com>
2024-12-12 00:47:38 +0900

    partial: function, replace: true인 경우 확인

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import FullStateSubscriber from './FullStateSubscriber';
 import CountSubscriber from './CountSubscriber';
 import Actions from './Actions';
+import useStore from './useStore'; // Zustand 스토어 import
 
 const App = () => {
+  const states = useStore(); // count 상태 구독
+  const setCount = useStore((state) => state.setCount); // setCount 함수 구독
+
   return (
+    <>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>state: {JSON.stringify(states)}</h1>
+      <button onClick={setCount}>Increment and Replace State</button>
+    </div>
     <div>
       <h1>Zustand Selector Test</h1>
       <Actions />
       <FullStateSubscriber />
       <CountSubscriber />
     </div>
+    </>
   );
 };
 
--- src/useStore.js
+++ src/useStore.js
@@ -2,9 +2,9 @@ import { create } from 'zustand';
 
 console.log(`create: \n\n${create()}`)
 
-const useStore = create((setState, getState, getInitialState) => {
-  console.log(`set :\n\n${setState}`)
-  console.log(`get :\n\n${getState}`)
+const useStore = create((set, get, getInitialState) => {
+  console.log(`set :\n\n${set}`)
+  console.log(`get :\n\n${get}`)
   console.log(`init :\n\n${getInitialState}`)
 
   return {
@@ -12,6 +12,9 @@ const useStore = create((setState, getState, getInitialState) => {
   text: '', // 상태 2
   increaseCount: () => set((state) => ({ count: state.count + 1 })), // count 증가
   updateText: (newText) => set({ text: newText }), // text 업데이트
+  // 함수로 상태 변경 및 교체
+  setCount: () =>
+  set((state) => ({ count: state.count + 1 }), true),
 }});
 
 export default useStore;
